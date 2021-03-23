c3f38b65fbaaa53a75ebe22da1a8108d987c7c9b
Ying Lin <yinglonglin@berkeley.edu>
2021-03-22 20:15:00 -0700

    changed test script to script first few items on nike

--- test.py
+++ test.py
@@ -1,28 +1,6 @@
-import requests
-#from bs4 import BeautifulSoup
-#from selenium import webdriver
-
-"""
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-#chrome_options.add_argument("--disable-extensions")
-#chrome_options.add_argument("--disable-gpu")
-#chrome_options.add_argument("--no-sandbox") # linux only
-chrome_options.add_argument("--headless")
-# chrome_options.headless = True # also works
-driver = webdriver.Chrome(options=chrome_options)
-start_url = "https://duckgo.com"
-driver.get(start_url)
-print(driver.page_source.encode("utf-8"))
-driver.quit()
-"""
-
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-
 from selenium import webdriver
 
 chrome_options = Options()
@@ -35,4 +13,29 @@ url = "https://www.nike.com/w/sale-3yaep"
 driver.get(url)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 driver.quit()
-print(soup.prettify())
+#print(soup.prettify())
+
+
+body = soup.body
+content = body.find('div', class_="product-grid__items")
+
+def retrieve_product_info(soup_object):
+    name = product.find('div', class_='product-card__title').string
+    link = product.find('a', class_='product-card__img-link-overlay')['href']
+    current_price = product.find('div', class_='is--current-price').string
+    past_price = product.find('div', class_='is--striked-out').string
+    return [name, link, current_price, past_price]
+
+for product in content:
+    try:
+        info_list = retrieve_product_info(product)
+        print("Name: " + info_list[0])
+        print("Link: " + info_list[1])
+        print("Current Price: " + info_list[2])
+        print("Past Price: " + info_list[3])
+        print()
+    except Exception as e:
+        pass
+
+
+#print(len(content.contents))
