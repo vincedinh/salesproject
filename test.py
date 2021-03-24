b0b8e69cf8b29e8247b38dc34aa104419454ea03
Ying Lin <yinglonglin@berkeley.edu>
2021-03-23 17:07:19 -0700

    changed nike code

--- test.py
+++ test.py
@@ -3,39 +3,40 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
+###CODE FOR SCRAPING USING SELENIUM###
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-chrome_options.add_argument('window-size=1920x1080')
+chrome_options.add_argument('window-size=1920x1080000')
 chrome_options.add_argument('--lang=en_US')
 driver = webdriver.Chrome(executable_path='./chromedriver', options=chrome_options)
-#url = "https://www.gap.com/browse/category.do?cid=65289&nav=meganav%3ASale%3AShop%20Sale%3AMen&ak_t=80597F6050F6CEB9FFBDDFFB91DBA2B517C532EEEA61000020804560A4BB1155"
 url = "https://www.nike.com/w/sale-3yaep"
 driver.get(url)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 driver.quit()
-#print(soup.prettify())
 
 
+###FINDING PRODUCTS ON THE WEBPAGE###
 body = soup.body
 content = body.find('div', class_="product-grid__items")
 
 def retrieve_product_info(soup_object):
-    name = product.find('div', class_='product-card__title').string
-    link = product.find('a', class_='product-card__img-link-overlay')['href']
-    current_price = product.find('div', class_='is--current-price').string
-    past_price = product.find('div', class_='is--striked-out').string
-    return [name, link, current_price, past_price]
+    product_dict = {}
+    product_dict["name"] = product.find('div', class_='product-card__title').string
+    product_dict["link"] = product.find('a', class_='product-card__img-link-overlay')['href']
+    product_dict["current_price"] = product.find('div', class_='is--current-price').string
+    product_dict["past_price"] = product.find('div', class_='is--striked-out').string
+    return product_dict
 
 for product in content:
     try:
-        info_list = retrieve_product_info(product)
-        print("Name: " + info_list[0])
-        print("Link: " + info_list[1])
-        print("Current Price: " + info_list[2])
-        print("Past Price: " + info_list[3])
+        product_dict = retrieve_product_info(product)
+        print("Name: " + product_dict["name"])
+        print("Link: " + product_dict["link"])
+        print("Current Price: " + product_dict["current_price"])
+        print("Past Price: " + product_dict["past_price"])
         print()
     except Exception as e:
         pass
 
 
-#print(len(content.contents))
+print(len(content.contents))
