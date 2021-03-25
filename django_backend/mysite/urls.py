be6deed13bd212169005ff59cb073599d21ec4a7
Ying Lin <yinglonglin@berkeley.edu>
2021-03-24 18:09:37 -0700

    changed names of files

--- django_backend/mysite/urls.py
+++ django_backend/mysite/urls.py
@@ -1,4 +1,4 @@
-"""django_backend URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
