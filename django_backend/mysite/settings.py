2e8615bb88f46aa566ac08622b27efdde07872b1
Ying Lin <yinglonglin@berkeley.edu>
2021-03-24 19:53:45 -0700

    settings changes

--- django_backend/mysite/settings.py
+++ django_backend/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#1fy9^c*$$=75spalj*1hw)e@(y2qy2ayl3ol024y#5gt7btsa'
+SECRET_KEY = None
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
