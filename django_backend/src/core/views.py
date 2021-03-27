f834c9cc2275626646cacf5d8a35bfe0f87b0c78
Ying Lin <yinglonglin@berkeley.edu>
2021-03-26 19:10:25 -0700

    set up django rest framework

--- django_backend/src/core/views.py
+++ django_backend/src/core/views.py
@@ -1,3 +1,24 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name': 'john',
+            'age': 23
+        }
+        return Response(data)
+
+
+
+"""
+def test_view(request):
+    data = {
+        'name': 'john',
+        'age': 23
+    }
+    return JsonResponse(data)
+"""
